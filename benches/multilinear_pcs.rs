41e6019d54fdec9a6c7dc0b0f7140388512185fb
Yanpei Guo <2847399255@qq.com>
2023-07-14 14:46:24 +0800

    create benches

--- benches/multilinear_pcs.rs
+++ benches/multilinear_pcs.rs
@@ -7,7 +7,7 @@ use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
 use merlin::Transcript;
 
 criterion_main!(bench);
-criterion_group!(bench, prove);
+criterion_group!(bench, benches);
 
 fn u8_to_scalar(x: u8) -> Scalar {
     let mut bits = [0; 32];
@@ -26,7 +26,7 @@ fn inner_product(a: &[Scalar], b: &[Scalar]) -> Scalar {
     out
 }
 
-fn f<const LOG_N: usize>() {
+fn prove<const LOG_N: usize>(criterion: &mut Criterion) {
     let vector_len = 1 << LOG_N;
     let mut rng = rand::thread_rng();
     let bp_gens = BulletproofGens::new(vector_len, 1);
@@ -58,26 +58,103 @@ fn f<const LOG_N: usize>() {
     )
     .compress();
 
-    LinearProof::create(
-        &mut transcript,
-        &mut rng,
-        &C,
-        r,
-        a.clone(),
-        b.clone(),
-        G.clone(),
-        &F,
-        &B,
-    )
-    .unwrap();
+    let mut group = criterion.benchmark_group("prove");
+    group.sample_size(10);
+    group.bench_function("open point", move |bencher| {
+        bencher.iter(|| {
+            LinearProof::create(
+                &mut transcript,
+                &mut rng,
+                &C,
+                r,
+                a.clone(),
+                b.clone(),
+                G.clone(),
+                &F,
+                &B,
+            )
+            .unwrap();
+        })
+    });
 }
 
-fn prove(c: &mut Criterion) {
-    let mut g = c.benchmark_group("prove");
-    g.sample_size(10);
-    g.bench_function("open point", move |b| {
-        b.iter(|| {
-            f::<16>();
+fn benches(c: &mut Criterion) {
+    prove::<16>(c);
+    verify::<16>(c);
+}
+
+fn verify<const LOG_N: usize>(criterion: &mut Criterion) {
+    let vector_len = 1 << LOG_N;
+    let bp_gens = BulletproofGens::new(vector_len, 1);
+    let mut rng = rand::thread_rng();
+
+    // Calls `.G()` on generators, which should be a pub(crate) function only.
+    // For now, make that function public so it can be accessed from benches.
+    // We can't simply use bp_gens directly because we don't need the H generators.
+    let G: Vec<RistrettoPoint> = bp_gens.share(0).G(vector_len).cloned().collect();
+    let pedersen_gens = PedersenGens::default();
+    let F = pedersen_gens.B;
+    let B = pedersen_gens.B_blinding;
+
+    let point = (0..LOG_N)
+        .map(|_| Scalar::random(&mut rng))
+        .collect::<Vec<_>>();
+    let mut b = vec![u8_to_scalar(1)];
+    for i in &point {
+        let len = b.len();
+        for j in 0..len {
+            b.push(b[j] * i);
+        }
+    }
+
+    // Generate the proof in its own scope to prevent reuse of
+    // prover variables by the verifier
+    let (proof, C) = {
+        // a and b are the vectors for which we want to prove c = <a,b>
+        let a: Vec<_> = (0..vector_len).map(|_| Scalar::random(&mut rng)).collect();
+
+        let mut transcript = Transcript::new(b"LinearProofBenchmark");
+
+        // C = <a, G> + r * B + <a, b> * F
+        let r = Scalar::random(&mut rng);
+        let c = inner_product(&a, &b);
+        let C = RistrettoPoint::vartime_multiscalar_mul(
+            a.iter().chain(iter::once(&r)).chain(iter::once(&c)),
+            G.iter().chain(iter::once(&B)).chain(iter::once(&F)),
+        )
+        .compress();
+
+        let proof = LinearProof::create(
+            &mut transcript,
+            &mut rng,
+            &C,
+            r,
+            a.clone(),
+            b.clone(),
+            G.clone(),
+            &F,
+            &B,
+        )
+        .unwrap();
+
+        (proof, C)
+    };
+
+    let mut group = criterion.benchmark_group("prove");
+    group.sample_size(10);
+    group.bench_function("verify point value", move |bencher| {
+        bencher.iter(|| {
+            let mut verifier_transcript = Transcript::new(b"LinearProofBenchmark");
+            let mut b = vec![u8_to_scalar(1)];
+            for i in &point {
+                let len = b.len();
+                for j in 0..len {
+                    b.push(b[j] * i);
+                }
+            }
+            proof
+                .verify(&mut verifier_transcript, &C, &G, &F, &B, b)
+                .unwrap();
         })
     });
 }
